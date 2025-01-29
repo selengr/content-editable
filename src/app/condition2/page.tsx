861ed481370806d2e27c00bb01bbe742ffba2a4e
selengr <reza1997karbakhsh@gmail.com>
2025-01-29 23:35:28 +0330

    fux

--- src/app/condition2/page.tsx
+++ src/app/condition2/page.tsx
@@ -406,13 +406,7 @@ export default function DependentSelectForm() {
    
     const transformInputToOutput = (input) => {
       return input.conditions.map(condition => {
-          // const conditionFormula = condition.subConditions.map(subCondition => {
-          //   if(!!subCondition?.logicalOperator){
-          //       return `${subCondition.conditionType}(${subCondition.questionType.split("*")[1]+","+subCondition.value})`;
-          //   } else {
-          //     return `${subCondition.conditionType}(${subCondition.questionType.split("*")[1]+","+subCondition.value})` + " " + subCondition?.logicalOperator + " ";
-          //   }
-          // }); 
+
           const conditionFormula = condition.subConditions.map(subCondition => {
             const baseCondition = `${subCondition.conditionType}(${subCondition.questionType.split("*")[1]},${subCondition.value})`;
             
