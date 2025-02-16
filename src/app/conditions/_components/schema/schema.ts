36f4a1227191e3e955d903ec504f5db2b4f46898
selengr <reza1997karbakhsh@gmail.com>
2025-02-16 23:15:39 +0330

    fix

--- src/app/conditions/_components/schema/schema.ts
+++ src/app/conditions/_components/schema/schema.ts
@@ -1,25 +1,25 @@
-// import { z } from "zod"
+import { z } from "zod"
 
-// export const SubConditionSchema = z.object({
-//   logicalOperator: z.enum(["AND", "OR"]),
-//   questionType: z.string(),
-//   operatorType: z.string(),
-//   conditionType: z.string(),
-//   value: z.string(),
-//   id: z.string().optional(),
-// })
+export const SubConditionSchema = z.object({
+  logicalOperator: z.enum(["AND", "OR"]),
+  questionType: z.string(),
+  operatorType: z.string(),
+  conditionType: z.string(),
+  value: z.string(),
+  id: z.string().optional(),
+})
 
-// export const ConditionSchema = z.object({
-//   subConditions: z.array(SubConditionSchema),
-//   elseQuestionId: z.string(),
-//   returnQuestionId: z.string(),
-// })
+export const ConditionSchema = z.object({
+  subConditions: z.array(SubConditionSchema),
+  elseQuestionId: z.string(),
+  returnQuestionId: z.string(),
+})
 
-// export const ConditionFormSchema = z.object({
-//   conditions: z.array(ConditionSchema),
-// })
+export const ConditionFormSchema = z.object({
+  conditions: z.array(ConditionSchema),
+})
 
-// export type SubCondition = z.infer<typeof SubConditionSchema>
-// export type Condition = z.infer<typeof ConditionSchema>
-// export type TConditionFormData = z.infer<typeof ConditionFormSchema>
+export type SubCondition = z.infer<typeof SubConditionSchema>
+export type Condition = z.infer<typeof ConditionSchema>
+export type TConditionFormData = z.infer<typeof ConditionFormSchema>
 
