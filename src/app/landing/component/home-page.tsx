d3a4c96ff578569e49c321bf8dd16fb9442aedae
selengr <reza1997karbakhsh@gmail.com>
2025-02-26 00:50:42 +0330

    check responsive

--- src/app/landing/component/home-page.tsx
+++ src/app/landing/component/home-page.tsx
@@ -1,27 +1,41 @@
-import { Button } from "@mui/material"
-import Image from "next/image"
-import Link from "next/link"
+import { Button } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen bg-white text-right md:pt-20 md:pr-6 md:pl-12" dir="rtl">
+    <main
+      className="min-h-screen bg-white text-right md:pt-20 md:pr-6 md:pl-12"
+      dir="rtl"
+    >
       <header className="mx-auto px-4 pt-6 flex justify-center md:hidden">
-        <Image src="/logo/logo.svg" alt="سایا لوگو" width={120} height={40} priority />
+        <Image
+          src="/logo/logo.svg"
+          alt="سایا لوگو"
+          width={120}
+          height={40}
+          priority
+        />
       </header>
 
       <div className="flex flex-col md:flex-row justify-start">
         <section className="px-4 text-center md:text-right">
-          <h1 className="text-3xl font-bold mb-2 text-[#4A4A4A]">دستیار هوشمند شناخت</h1>
+          <h1 className="text-3xl font-bold mb-2 text-[#4A4A4A]">
+            دستیار هوشمند شناخت
+          </h1>
           <div className="flex flex-col justify-center md:flex-row items-center">
             <div
               className="h-[2px] w-10 md:w-9 rounded-full bg-[#2CDFC9] mx-auto my-3 md:mb-5 md:mx-2"
               aria-hidden="true"
             ></div>
-            <h2 className="text-[22px] font-semibold text-[#FA4D56] mb-2">چه کاری انجام می‌دهیم!</h2>
+            <h2 className="text-[22px] font-semibold text-[#FA4D56] mb-2">
+              چه کاری انجام می‌دهیم!
+            </h2>
           </div>
           <p className="text-[14px] text-[#4A4A4A] mx-auto md:mx-0 leading-relaxed mb-4 px-[20px] md:px-0 max-w-72">
-            سایا سکویی برای ساخت، اجرا و تحلیل آزمون‌های روان‌شناختی است. این سکو با رابط کاربری ساده و یکپارچه، امکان
-            ایجاد فرم‌های برخط و گزارش‌های شخصی‌سازی شده را فراهم می‌کند.
+            سایا سکویی برای ساخت، اجرا و تحلیل آزمون‌های روان‌شناختی است. این
+            سکو با رابط کاربری ساده و یکپارچه، امکان ایجاد فرم‌های برخط و
+            گزارش‌های شخصی‌سازی شده را فراهم می‌کند.
           </p>
         </section>
 
@@ -57,10 +71,41 @@ export default function HomePage() {
             aria-hidden="true"
           ></div>
 
-         <div className="w-[100px] h-[100px] absolute ">
-         <div className="bg-circle-bg bg- bg-center bg-no-repeat min-h-[350px] w-[80%]"></div>
-         </div>
-
+          <Image
+            src="/images/home-page/circle-bg.svg"
+            alt="سایا لوگو"
+            width={100}
+            height={100}
+            className="w-full min-h-[300px] h-full absolute  -top-6 -left-36"
+          />
+          <Image
+            src="/images/home-page/Oval2.svg"
+            alt="object"
+            width={50}
+            height={28}
+            className="w-[60%] h-[60%] absolute -top-7 left-32"
+          />
+          <Image
+            src="/images/home-page/Oval1.svg"
+            alt="object"
+            width={50}
+            height={28}
+            className="w-[60%] h-[60%] absolute -top-[85px] -left-[165px]"
+          />
+          <Image
+            src="/images/home-page/Oval3.svg"
+            alt="object"
+            width={50}
+            height={28}
+            className="w-[60%] h-[60%] absolute -bottom-[65px] -left-[105px]"
+          />
+          <Image
+            src="/images/home-page/Oval4.svg"
+            alt="object"
+            width={50}
+            height={28}
+            className="w-[60%] h-[60%] absolute -bottom-[100px] -right-[135px]"
+          />
           <div className="flex flex-row gap-12 justify-center min-h-[200px] w-full items-center h-full p-6 bg-[#FAFAFA] rounded-[60px]">
             <FormBuilderCard />
             <PublicFormsCard />
@@ -69,36 +114,62 @@ export default function HomePage() {
       </div>
 
       <footer className="container flex flex-col items-center justify-center mx-auto px-4 py-12 text-center bg-banner-bg2 bg-cover bg-center bg-no-repeat -top-32 h-[380px] max-h-[380px] max-w-96 relative">
-        <Image src="/logo/logo.svg" alt="سایا لوگو" width={120} height={40} className="mb-8 hidden md:block" />
-        <span className="text-[14px] text-[#4A4A4A] mt-4">به آموزش بیشتری نیاز دارید؟</span>
+        <Image
+          src="/logo/logo.svg"
+          alt="سایا لوگو"
+          width={120}
+          height={40}
+          className="mb-8 hidden md:block"
+        />
+        <span className="text-[14px] text-[#4A4A4A] mt-4">
+          به آموزش بیشتری نیاز دارید؟
+        </span>
       </footer>
     </main>
-  )
+  );
 }
 
 function FormBuilderCard() {
   return (
-    <div className="bg-[linear-gradient(233.47deg,_#2CDFC9_-51.3%,_#1758BA_86.56%)] text-white rounded-[45px] min-h-[190px] shadow-lg pt-8 pr-4 w-[100%] max-w-[160px]  relative">
+    <div className="bg-[linear-gradient(233.47deg,_#2CDFC9_-51.3%,_#1758BA_86.56%)] text-white relative rounded-[45px] h-[190px] shadow-xl pt-8 pr-4 w-[100%] max-w-[160px]">
       <h3 className="text-md font-bold mb-4">فرم ساز</h3>
       <p className="mb-6 text-xs">ساخت حرفه‌ای فرم با قابلیت درگ اند دراپ</p>
-      <Button className="w-full justify-center absolute bottom-5 right-9 text-[9px] text-white">
-        <Link href="#">ورود به فرم ساز</Link>
+      <Button className="w-full justify-center absolute bottom-0 left-0 text-[9px] text-white">
+        <Link className="text-white" href="#">ورود به فرم ساز</Link>
+        <div className="bg-[#fff] rounded-[12px] w-[30px] h-[30px] flex items-center justify-center mr-2">
+          <Image
+            src="/images/home-page/arrow-left.svg"
+            alt="object"
+            width={6}
+            height={10}
+          />
+        </div>
       </Button>
     </div>
-  )
+  );
 }
 
 function PublicFormsCard() {
   return (
-    <div className="bg-white rounded-[45px] shadow-lg pt-8  min-h-[190px] w-[100%] max-w-[160px] relative">
-      <h3 className="text-md font-bold text-[#1758BA] pr-4 mb-2">فرم‌های عمومی</h3>
-      <p className="text-[#2A2A2A] mb-6 text-xs pr-3 pl-4">مجموعه‌ای از فرم‌های منتشر شده عمومی در سامانه امرسالت</p>
-      <Button
-        className="w-full justify-center absolute bottom-5 right-14 text-[9px] text-[#1758BA]"
-      >
+    <div className="bg-white rounded-[45px] shadow-2xl pt-8   w-[100%] max-w-[160px] h-[190px] relative shadow-[0px 73px 90px -38px rgba(0, 0, 0, 0.1895)]">
+      <h3 className="text-md font-bold text-[#1758BA] pr-4 mb-2">
+        فرم‌های عمومی
+      </h3>
+      <p className="text-[#2A2A2A] mb-6 text-xs pr-3 pl-4">
+        مجموعه‌ای از فرم‌های منتشر شده عمومی در سامانه امرسالت
+      </p>
+      <Button className="w-full justify-center absolute bottom-0 -left-6 text-[9px] text-[#1758BA]">
         <Link href="#">مشاهده</Link>
+        <div className="bg-[#1758BA] rounded-[12px] w-[30px] h-[30px] flex items-center justify-center mr-2">
+          <Image
+            src="/images/home-page/arrow-left.svg"
+            alt="object"
+            width={6}
+            height={10}
+            className="flex items-center justify-center"
+          />
+        </div>
       </Button>
     </div>
-  )
+  );
 }
-
