672894de63763c559ab96bbdaa2a1ec9c6577d9b
selengr <reza1997karbakhsh@gmail.com>
2025-03-06 00:01:02 +0330

    fewf

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,20 @@
+
+
+export default function PaymentPage() {
+
+    return (
+        <div className="container mx-auto py-8 flex justify-center items-center h-screen bg-white">
+
+            <div className="w-[70%] p-2">
+
+                    <div className="bg-[#F7F7FF] rounded-lg w-ful">
+                            <h3 className="text-[#161616] font-bold">سبد خرید</h3>
+                    </div>
+            </div>
+
+            <div className="w-[30%]">
+
+            </div>
+         </div>
+    )
+}
