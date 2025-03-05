56a101386de9ce99ed241d292a05936e561a46b3
selengr <reza1997karbakhsh@gmail.com>
2025-03-06 00:07:19 +0330

    update

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -7,9 +7,18 @@ export default function PaymentPage() {
 
             <div className="w-[70%] p-2">
 
-                    <div className="bg-[#F7F7FF] rounded-lg w-ful">
-                            <h3 className="text-[#161616] font-bold">سبد خرید</h3>
+                   <div className="w-full justify-center items-center">
+                   <div className="bg-[#F7F7FF] rounded-lg w-ful h-[52px text-center] mb-5">
+                            <h3 className="text-[#161616] font-bold text-base">سبد خرید</h3>
                     </div>
+                    <div className="px-16">
+                        {[1,2,3,4,5].map(()=>(
+                            <div className="rounded-[20px] border-[1px] border-[#DDE1E6] h-[84px]">
+
+                            </div>
+    ))}
+                    </div>
+                   </div>
             </div>
 
             <div className="w-[30%]">
