178f6547783d86d01f037cf845e31e53db5c00b8
selengr <reza1997karbakhsh@gmail.com>
2025-06-26 15:58:04 +0330

    update drop

--- src/app/dnd/_components/KanbanBoard.tsx
+++ src/app/dnd/_components/KanbanBoard.tsx
@@ -1,5 +1,7 @@
 
 import { TaskCard } from './TaskCard';
+import {useDroppable} from '@dnd-kit/core';
+
 import { Column as ColumnType, Task } from './types';
 
 type ColumnProps = {
@@ -8,12 +10,14 @@ type ColumnProps = {
 };
 
 export function KanbanBoard({ column, tasks }: ColumnProps) {
-
+  const {isOver, setNodeRef} = useDroppable({
+    id: column.id,
+  });
 
   return (
     <div className="flex w-80 flex-col rounded-lg bg-neutral-800 p-4 h-screen">
       <h2 className="mb-4 font-semibold text-neutral-100">{column.title}</h2>
-      <div className="flex flex-1 flex-col gap-4 ">
+      <div ref={setNodeRef} className="flex flex-1 flex-col gap-4 ">
         {tasks.map((task) => {
           return <TaskCard key={task.id} task={task} />;
         })}
--- src/app/dnd/_components/TaskCard.tsx
+++ src/app/dnd/_components/TaskCard.tsx
@@ -1,15 +1,28 @@
 import { Task } from './types';
+import {useDraggable} from '@dnd-kit/core'
 
 type TaskCardProps = {
   task: Task;
 };
 
 export function TaskCard({ task }: TaskCardProps) {
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+        id: task.id,
+      });
 
+      const style = transform
+      ? {
+          transform: `translate(${transform.x}px, ${transform.y}px)`,
+        }
+      : undefined;
 
   return (
     <div
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
       className="cursor-grab rounded-lg bg-neutral-700 p-4 shadow-sm hover:shadow-md"
+      style={style}
     >
       <h3 className="font-medium text-neutral-100">{task.title}</h3>
       <p className="mt-2 text-sm text-neutral-400">{task.description}</p>
